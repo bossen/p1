8bda2dee2c0e4057f5ec29dd886d1374e829a716
Olavur Debes Joensen <olavur.debes@gmail.com>
2012-11-29 12:22:39 +0100

    Program opdateret

--- Program/charcount.c
+++ Program/charcount.c
@@ -1,22 +1,34 @@
 #include <stdio.h>
 #define PRINTBOUNDARY 32
 #define MAXCHARS 256
+
+typedef struct {
+    char symbol;
+    unsigned long int amount;
+} entry;
+
+int entryCompare(const void *e1, const void *e2)
+{
+    return ((entry *)e2)->amount - ((entry *)e1)->amount;
+}
+
 int main(void)
 {
     // Variable
     int i, curr, charcount = MAXCHARS - PRINTBOUNDARY;
-    unsigned long int array[charcount];
+    entry array[charcount];
     // Initialisering af array
-    for(i = 0; i < charcount; i++)
-        array[i] = 0;
-    
+    for(i = 0; i < charcount; i++) {
+        array[i].symbol = i + PRINTBOUNDARY;
+        array[i].amount = 0;
+    }
     do
     {
         curr = getchar();
-        array[(curr - PRINTBOUNDARY)]++;
+        array[(curr - PRINTBOUNDARY)].amount++;
     } while (curr != EOF);
-    
+    qsort(array, charcount, sizeof(entry), entryCompare);
     FILE *result = fopen("occurrences.txt", "w");
     for(i = 0; i < charcount; i++)
-        fprintf(result, "%c : %d\n", i + PRINTBOUNDARY, array[i]);
+        fprintf(result, "%c : %d\n", array[i].symbol, array[i].amount);
 }
